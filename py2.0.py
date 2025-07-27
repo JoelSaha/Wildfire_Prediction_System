c27d75ac3621c9ffd44cf07666a47410c64c051f
Joel Saha <157637764+JoelSaha@users.noreply.github.com>
2025-07-27 18:33:00 +0530

    Update py2.0.py

--- py2.0.py
+++ py2.0.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # The group name and the message to send
-group_name = "Jayanagar"
+group_name = "" #Area name
 message = "90%"
 
 # Call the Node.js script, passing group name and message as arguments
